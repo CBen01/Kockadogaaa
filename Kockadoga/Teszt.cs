16eb507ae727ae93bd51a8db6d9b6cc30d95e875
Benjámin Bucsa <benike774@gmail.com>
2023-11-15 17:57:06 +0100

    Add project files.

---
+++ Kockadoga/Teszt.cs
@@ -0,0 +1,26 @@
+﻿using Negyszog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kockadoga
+{
+    class Teszt
+    {
+        static void Main(string[] args)
+        {
+
+            NegyszogClass sz = new NegyszogClass(3, 5);
+            sz.SetKerulet();
+            sz.SetTerulet();
+
+
+            Console.WriteLine("A négyszög kerülete: {0:0.00}, területe {1:0.00}", sz.getKerulet(), sz.getTerulet());
+
+            Console.ReadKey();
+
+        }
+    }
+}
